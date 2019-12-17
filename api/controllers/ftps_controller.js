eca0659068cf72f352cbd500a6b3e0da7aee27c1
Сергій <sergio>
2019-12-17 21:27:48 +0200

    init commit

---
+++ api/controllers/ftps_controller.js
@@ -0,0 +1,231 @@
+var config = require('config')
+  , _ = require('lodash')
+  , Jsftp = require('jsftp')
+  , mime = require('mime')
+  , fs = require('fs')
+  , async = require('async');
+
+function quitConn(myFtp) {
+  myFtp.raw.quit(function (err, data) {
+    if (err) return;
+  });
+}
+var ftps = {};
+if (!(fs.existsSync("./temp"))) {
+  fs.mkdirSync('./temp');
+}
+function getCookie(name, cookies) {
+  var list = {},
+    rc = cookies;
+  rc && rc.split(';').forEach(function (cookie) {
+    var parts = cookie.split('=');
+    list[parts.shift().trim()] = decodeURI(parts.join('='));
+  });
+  return list[name];
+}
+
+ftps.connect = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.body);
+
+
+    ftp.auth(req.body.user, req.body.pass, function (err, result) {
+      quitConn(ftp)
+      if (err) return next(err)
+      else if (result && !result.isError) {
+        req.session.ftp = req.body
+        res.json(req.body);
+      } else return next({ errors: [result.text] })
+    })
+
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+
+  }
+};
+ftps.upload = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  const file = req.files.uploadfile;
+  // async.each(req.files.uploadfile, function (file, callback) {
+    var filePath = file.path;
+    var filename = file.originalFilename;
+    var fileData = fs.readFileSync(filePath);
+    ftp.put(fileData, req.body.dir + filename, function (hadError) {
+      res.json({ message: 'Files Uploaded Successfully' })
+    });
+  // }, function (err) {
+  //   quitConn(ftp)
+  //   if (err) return next(err)
+  //   else res.json({ message: 'Files Uploaded Successfully' })
+  // })
+}
+ftps.list = function (req, res, next) {
+
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  ftp.ls(req.body.dir || '/', function (err, files) {
+    quitConn(ftp)
+    if (err) return next(err)
+    else {
+      files = _.sortBy(files, function (file) {
+        return - file.type;
+      })
+      files = _.map(files, function (file) {
+        file.time = new Date(file.time);
+        var perm = '';
+        ['userPermissions', 'groupPermissions', 'otherPermissions'].forEach(function (permission_type) {
+          if (file.hasOwnProperty(permission_type)) {
+            ['read', 'write', 'exec'].forEach(function (permission) {
+              var keyword;
+              if (permission == 'read') keyword = 'r'
+              else if (permission == 'write') keyword = 'w'
+              else keyword = 'x'
+              if (file[permission_type][permission]) perm += keyword
+              else perm += '-'
+            })
+          }
+        })
+        if (file.type != 0) {
+          file.type = 'Directory';
+        } else {
+          file.type = 'File';
+        }
+        file.perm = perm;
+        return file;
+      })
+      var currentDir = req.body.dir || '/';
+      currentDir = currentDir.split('/');
+
+      res.json({ ftpdirlist: files, dirs: currentDir });
+    }
+  })
+}
+ftps.download = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+
+  var uniquekey = getCookie('connect.sid', req.headers.cookie)
+  var localFilePath = './temp/' + uniquekey + '_' + req.query.name;
+  req.query.path = (req.query.path.charAt(req.query.path) == '/') ? req.query.path : req.query.path + '/';
+  ftp.get(req.query.path + req.query.name, localFilePath, function (err) {
+    if (err) next({ errors: [err], status: 500 })
+    else {
+      var mimetype = mime.lookup(localFilePath);
+      res.setHeader('Content-disposition', 'attachment; filename=' + req.query.name);
+      res.setHeader('Content-type', mimetype);
+      var filestream = fs.createReadStream(localFilePath);
+      filestream.pipe(res);
+      fs.unlinkSync(localFilePath)
+    }
+    quitConn(ftp);
+  })
+}
+ftps.rename = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  var from = req.body.path + req.body.source;
+  var to = req.body.path + req.body.dest
+  ftp.rename(from, to, function (err, data) {
+    quitConn(ftp)
+    if (err) return next(err)
+    else res.json({ message: 'Rename successful' })
+  })
+}
+ftps.mkdir = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  ftp.raw.mkd(req.body.dir, function (err, data) {
+    if (err) return next(err)
+    else res.json({ message: 'Directory Created' })
+  })
+}
+ftps.delete = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  ftp.raw.dele(req.body.path + req.body.name, function (err, data) {
+    if (err) return next(err)
+    else res.json({ message: 'File Deleted' })
+  })
+}
+ftps.move = function () {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  var from = req.body.sourcePath + req.body.sourceFile;
+  var to = req.body.sourcePath + req.body.destFile
+  ftp.rename(from, to, function (err, res) {
+    quitConn(ftp)
+    if (err) return next(err)
+    else res.json({ message: 'Rename successful' })
+  })
+}
+ftps.copy = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.session.ftp);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  if (!req.body.sourcePath) {
+    return next({ errors: ['source path '] })
+  } else if (!req.body.destPath) {
+    return next({ errors: ['source path '] })
+  } else if (req.body.sourcePath == req.body.destPath) {
+    return next({ errors: ['Source path & dest path could not be same'] })
+  }
+  var uniquekey = getCookie('connect.sid', req.headers.cookie)
+  var localFilePath = './temp/' + uniquekey + '_' + req.body.name;
+  ftp.get(req.body.sourcePath + req.body.name, localFilePath, function (err, socket) {
+    if (err) {
+      quitConn(ftp)
+      return next(err)
+    }
+    else {
+      ftp.put(localFilePath, req.body.destPath + req.body.name, function (err) {
+        quitConn(ftp)
+        if (err) return next(err)
+        else res.json({ message: 'Copied File Successfully' })
+        fs.unlinkSync(localFilePath)
+      })
+    }
+  });
+}
+ftps.testConnection = function (req, res, next) {
+  try {
+    var ftp = new Jsftp(req.body);
+  } catch (e) {
+    return next({ errors: [e], status: 500 })
+  }
+  ftp.auth(req.body.user, req.body.pass, function (err, result) {
+    quitConn(ftp)
+    if (err) return next(err)
+    else if (result && !result.isError) {
+      req.session.ftp = req.body
+      res.json(req.body);
+    } else return next({ errors: [text] })
+  })
+};
+
+
+
+module.exports = ftps;
---
+++ public/js/script.js
@@ -0,0 +1,443 @@
+var TemplateManager = {
+  templates: {}, // holds the templates cache
+  get: function (id, callback) {
+    var template = this.templates[id];
+    if (template) { // return the cached version if it exists
+      callback(template);
+    } else {
+      var that = this;
+      $.get('/templates/' + id + ".hbs", function (template) {
+        that.templates[id] = template;
+        callback(that.templates[id]);
+      });
+    }
+  }
+}
+
+function parseQueryString(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+
+}
+Handlebars.registerHelper("when", function (operand_1, operator, operand_2, options) {
+  var operators = {
+    'eq': function (l, r) {
+      return l == r;
+    },
+    'noteq': function (l, r) {
+      return l != r;
+    },
+    'gt': function (l, r) {
+      return Number(l) > Number(r);
+    },
+    'or': function (l, r) {
+      return l || r;
+    },
+    'and': function (l, r) {
+      return l && r;
+    },
+    '%': function (l, r) {
+      return (l % r) === 0;
+    }
+  },
+    result = operators[operator](operand_1, operand_2);
+
+  if (result) return options.fn(this);
+  else return options.inverse(this);
+});
+Handlebars.registerHelper("formattedDate", function (date) {
+  return new Date(date).toLocaleString()
+})
+Handlebars.registerHelper('fileIcon', function (target) {
+  var allowedExtensions = ["txt", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "bmp", "pdf", "gif", "jpeg", "jpg", "png", "bz2", "dmg", "gz", "gzip", "iso", "rar", "tar", "tgz", "zip"];
+
+  var targetNameParts = target.split(".");
+  var extensionPart = targetNameParts.length;
+  var extension =
+    ($.inArray(targetNameParts[extensionPart - 1], allowedExtensions) > -1) ?
+      targetNameParts[extensionPart - 1] : "txt";
+
+  return 'fa-file-' + extension + '-o';
+})
+Handlebars.registerHelper('humanFileSize', function (bytes) {
+  var si = true;
+  var thresh = si ? 1000 : 1024;
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B';
+  }
+  var units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  var u = -1;
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+  return bytes.toFixed(1) + ' ' + units[u];
+})
+var BaseModel = Backbone.Model.extend({});
+
+var FtpConnect = Backbone.Model.extend({
+  urlRoot: '/api/ftp/connect',
+  defaults: {
+    port: '21'
+  }
+});
+var FtpListModel = Backbone.Model.extend({
+  urlRoot: '/api/ftp/list',
+  defaults: {
+    dir: '/'
+  }
+});
+var renameModel = Backbone.Model.extend({
+  urlRoot: '/api/ftp/rename'
+});
+var deleteModel = Backbone.Model.extend({
+  urlRoot: '/api/ftp/delete'
+});
+
+
+var FtpListView = Backbone.View.extend({
+  el: '.page',
+
+  events: {
+    'click .list': 'listDirFile',
+    'click .path': 'goToPath',
+    'click .action': 'Action',
+    'click .mkdir': 'makeDir',
+    'click .delete': 'deleteFile'
+  },
+  Action: function (event) {
+    var data = $(event.currentTarget).data();
+    if (data.action == 'download') {
+      this.download(data.name)
+    } else if (data.action == 'rename') {
+      this.rename(data.name);
+    }
+  },
+
+  makeDir: function (event) {
+    var that = this;
+    var newdir = prompt("New Directory Name ", '');
+    if (newdir) {
+      var dir = this.model.get('dir') + '/' + newdir
+      var request = $.post('/api/ftp/mkdir', { dir: dir });
+
+      request.success(function (result) {
+        window.App.flash('Directory Created', 'success')
+        that.render();
+      });
+
+      request.error(function (jqXHR, textStatus, errorThrown) {
+        window.App.flash('Something went wrong', 'error')
+        // Etc
+      });
+    }
+  },
+  download: function (name) {
+    var currentDir = this.model.get('dir') || '/';
+
+  },
+  rename: function (name) {
+    var that = this;
+    var currentDir = this.model.get('dir') || '/';
+    var newfile = prompt("Rename ", name);
+
+    if (newfile && newfile != name) {
+      var rename = new renameModel({ path: currentDir, source: name, dest: newfile });
+      rename.save({}, {
+        success: function () {
+          window.App.flash("Rename successfull", 'success');
+          that.render();
+
+        }
+      })
+    }
+  },
+  deleteFile: function (event) {
+    var data = $(event.currentTarget).data();
+    if (confirm("Are you sure,do you want to delete this file ? " + data.name) != true) {
+      return false;
+    }
+    var that = this;
+
+    var delete_model = new deleteModel({ path: this.model.get('dir') || '/', name: data.name });
+    delete_model.save({}, {
+      success: function () {
+        window.App.flash("File Deleted", 'success');
+        that.render();
+      }
+    })
+    return false;
+
+  },
+  postRender: function () {
+    var that = this;
+    $("#uplodadfileform").on('submit', function () {
+      var formData = new FormData($(this)[0]);
+
+      $.ajax({
+        url: '/api/ftp/upload',
+        type: 'POST',
+        data: formData,
+        async: false,
+        success: function (data) {
+          window.App.flash('File Uploaded ', 'success')
+          that.render();
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+      });
+      return false;
+    });
+    this.$el.find("input:file").change(function () {
+      that.$el.find("#uplodadfileform").submit();
+    });
+  },
+  initialize: function () {
+    var that = this;
+    this.model = new FtpListModel({ dir: sessionStorage.getItem('currentDir') || '/' });
+    this.model.on('change:dir', function () {
+      window.sessionStorage.setItem('currentDir', that.model.get('dir'));
+    })
+
+  },
+  goToPath: function (event) {
+    var data = $(event.currentTarget).data();
+    var dirs = this.model.get('dirs') || [];
+    dirs = JSON.parse(JSON.stringify(dirs));
+    dirs.pop();
+    if (dirs.indexOf(data.name) >= 0) {
+      var dir = '';
+      for (var i = 0; i <= dirs.indexOf(data.name); i++) {
+        dir += dirs[i] + '/'
+      }
+      this.model.set('dir', dir);
+      this.render();
+    } else {
+
+    }
+  },
+  listDirFile: function (event) {
+    var data = $(event.currentTarget).data();
+    if (data.type == 'folder') {
+      var dir = this.model.get('dirs') || [];
+      dir = dir.join('/')
+      this.model.set('dir', dir + data.name + "/");
+      this.render();
+    } else {
+
+    }
+  },
+  render: function () {
+    var that = this;
+    this.model.save({}, {
+      success: function () {
+
+        TemplateManager.get('ftp_list', function (template) {
+
+          var template = Handlebars.compile(template);
+
+          var data = JSON.parse(JSON.stringify(that.model.attributes))
+          data.dirs.pop();
+          if (data.ftpdirlist) {
+            var total = data.ftpdirlist.length;
+
+            var directories = _.filter(data.ftpdirlist, { type: 'Directory' });
+            var dirtotal = directories.length;
+            var filetotal = total - dirtotal;
+            _.extend(data, { size_info: { total: total, dir: dirtotal, file: filetotal } }, { isMobile: App.isMobile })
+          }
+          var html = template(data);
+          that.$el.html(html);
+          that.postRender();
+        })
+      }.bind(this)
+    });
+    return this;
+  }
+});
+var NavBarView = Backbone.View.extend({
+
+  el: 'nav',
+  events: {
+    'click .signout': 'Logout'
+  },
+  initialize: function () {
+  },
+  Logout: function () {
+    var request = $.post('/api/ftp/logout', {});
+
+    request.success(function (result) {
+      window.sessionStorage.removeItem('currentDir');
+      window.App.flash('Logout Successfully', 'success')
+      window.location = '/';
+    });
+
+    request.error(function (jqXHR, textStatus, errorThrown) {
+      window.App.error('Something went wrong')
+      // Etc
+    });
+    return false;
+  },
+  render: function () {
+    TemplateManager.get('navbar', function (source) {
+      var template = Handlebars.compile(source);
+      var html = template({ ftp: window.App.User });
+      this.$el.html(html);
+    }.bind(this))
+  }
+})
+var HomeView = Backbone.View.extend({
+  el: '.page',
+  initialize: function () {
+    this._modelBinder = new Backbone.ModelBinder();
+    this.model = new FtpConnect()
+  },
+  events: {
+    'click .login': "Login"
+  },
+  render: function (id) {
+    if (window.sessionStorage.getItem('ftp')) {
+      this.model.set(JSON.parse(window.sessionStorage.getItem('ftp') || {}));
+    }
+
+    // this.model.unset('id');
+    var that = this;
+
+    TemplateManager.get('home', function (template) {
+      // var source = $('#user-info-template').html();
+      var template = Handlebars.compile(template);
+      var html = template(that.model.attributes || {});
+      that.$el.html(html);
+      that.bindModel()
+      return false;
+    })
+  },
+  addSettingModal: function (event) {
+    window.App.settingmodalview.render(this.model);
+    return false;
+  },
+  bindModel: function () {
+    var bindings = {
+      host: '[name=host]',
+      user: '[name=user]',
+      pass: '[name=pass]',
+      port: '[name=port]'
+    };
+    this._modelBinder.bind(this.model, this.el, bindings);
+  },
+  Login: function (e) {
+    var that = this;
+    that.model.save({}, {
+      success: function () {
+        // that.$el.modal('hide');
+        window.App.User = that.model.attributes;
+
+        window.sessionStorage.setItem('ftp', JSON.stringify({ host: that.model.get('host'), user: that.model.get('user') }))
+        window.location = '/'
+      },
+      error: function (err) {
+        window.App.flash("Something went wrong. Please try again", 'error');
+      }
+    })
+  }
+});
+
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'home',
+    'ftplist': 'index'
+
+  },
+  index: function () {
+    if (window.App.User) {
+      router.currentView = new FtpListView();
+      router.currentView.render();
+    } else {
+      App.router.navigate('', { trigger: true })
+    }
+  },
+  home: function () {
+    if (window.App.User) {
+      router.currentView = new FtpListView();
+      router.currentView.render();
+    } else {
+      router.currentView = new HomeView();
+      router.currentView.render();
+    }
+
+  },
+  execute: function (callback, args) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      App.isMobile = true;
+    }
+    var that = this;
+
+    window.App.loggedIn(function (err, user) {
+      if (!App.navBarView) {
+        App.navBarView = new NavBarView()
+        App.navBarView.render();
+      }
+      args.push(parseQueryString(args.pop()));
+      if (callback) callback.apply(that, args);
+    })
+
+  }
+});
+var router = new Router();
+window.App = window.App || {};
+window.App.router = router;
+window.App.setLoading = function (loading) {
+  $('body').toggleClass('loading', loading);
+
+  if (!loading) {
+    $('body').css('overflow-y', 'auto');
+  } else {
+    $('body').css('overflow-y', 'hidden');
+  }
+}
+window.App.loggedIn = function (callback) {
+  var request = $.get('/api/isloggedin');
+
+  request.success(function (result) {
+
+    window.App.User = result
+    callback(null, result);
+  });
+  request.error(function (jqXHR, textStatus, errorThrown) {
+    callback(errorThrown)
+  });
+}
+window.App.flash = function (message, type) {
+  if (type == 'error') {
+    $.growl.error({
+      message: message,
+      duration: 1000,
+      type: 'danger'
+    });
+  } else {
+    $.growl.notice({
+      message: message,
+      title: '',
+      duration: 1000
+    });
+  }
+}
+jQuery.ajaxSetup({
+  beforeSend: function () {
+    window.App.setLoading(true)
+  },
+  complete: function () {
+    window.App.setLoading(false)
+  },
+  success: function () { }
+});
+Backbone.history.start();
